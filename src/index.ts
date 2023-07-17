832888a0c5a29f203ccfb2601e52664ed64bf1fa
naaaah <107448004+naaaahTheDev@users.noreply.github.com>
2023-07-18 05:16:21 +0800

    Added index functionality
    New createIndex function.

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,70 @@
 import chalk from "chalk";
 import fs from "fs";
+import { getAllJSDocTagsOfKind } from "typescript";
 
 interface DataObject {
   [key: string]: any;
 }
 
+interface IndexObject {
+  [key: string]: number[];
+}
+
 export default {
-  push(filePath: string, dataCollectionName: string, data: { id?: number, [key: string]: any}): void {
+  push(filePath: string, dataCollectionName: string, data: { id?: number, [key: string]: any }): void {
     fs.readFile(filePath, 'utf8', (err, fileData) => {
       if (err) {
         if (err.code === 'ENOENT') {
-            //File does not exist, create the file
-            const initialData = {
-                [dataCollectionName]: []
-            };
-            const initialJsonData = JSON.stringify(initialData, null, 2);
+          //File does not exist, create the file
+          const initialData = {
+            [dataCollectionName]: []
+          };
+          const initialJsonData = JSON.stringify(initialData, null, 2);
 
-            fs.writeFile(filePath, initialJsonData, (writeError) => {
-                if (writeError) {
-                    console.error(chalk.red('Error creating JSON file:', writeError));
-                    return;
-                }
-                console.log(chalk.green('Database file created successfully.'));
-                //Call the push function recursively now that the file is created
-                this.push(filePath, dataCollectionName, data);
-            });
+          fs.writeFile(filePath, initialJsonData, (writeError) => {
+            if (writeError) {
+              console.error(chalk.red('Error creating JSON file:', writeError));
+              return;
+            }
+            console.log(chalk.green('Database file created successfully.'));
+            //Call the push function recursively now that the file is created
+            this.push(filePath, dataCollectionName, data);
+          });
         } else {
-            console.error(chalk.red('Error reading JSON file', err));
+          console.error(chalk.red('Error reading JSON file', err));
         }
       } else {
-      let dataObject: DataObject = {};
+        let dataObject: DataObject = {};
 
-      try {
-        if (fileData) {
-          dataObject = JSON.parse(fileData);
-        }
-      } catch (parseError) {
-        console.error(chalk.red('Error parsing JSON file:', parseError));
-        return;
-      }
-
-      if (!dataObject[dataCollectionName]) {
-        dataObject[dataCollectionName] = [];
-      }
-
-      const dataCollection = dataObject[dataCollectionName];
-
-      const id = data.id || dataCollection.length + 1;
-
-      dataCollection.push({ id, ...data });
-
-      const updatedJsonData = JSON.stringify(dataObject, null, 2);
-
-      fs.writeFile(filePath, updatedJsonData, (writeError) => {
-        if (writeError) {
-          console.error(chalk.red('Error writing JSON file:', writeError));
+        try {
+          if (fileData) {
+            dataObject = JSON.parse(fileData);
+          }
+        } catch (parseError) {
+          console.error(chalk.red('Error parsing JSON file:', parseError));
           return;
         }
-        console.log(chalk.green('New data has been added to the JSON file successfully!'));
-      });
-    }
+
+        if (!dataObject[dataCollectionName]) {
+          dataObject[dataCollectionName] = [];
+        }
+
+        const dataCollection = dataObject[dataCollectionName];
+
+        const id = data.id || dataCollection.length + 1;
+
+        dataCollection.push({ id, ...data });
+
+        const updatedJsonData = JSON.stringify(dataObject, null, 2);
+
+        fs.writeFile(filePath, updatedJsonData, (writeError) => {
+          if (writeError) {
+            console.error(chalk.red('Error writing JSON file:', writeError));
+            return;
+          }
+          console.log(chalk.green('New data has been added to the JSON file successfully!'));
+        });
+      }
     });
   },
 
@@ -69,6 +74,7 @@ export default {
         console.error(chalk.red(err));
         return;
       }
+
       let dataObject: DataObject = {};
 
       try {
@@ -78,6 +84,7 @@ export default {
       }
 
       const dataCollection = dataObject[dataCollectionName];
+
       if (!dataCollection) {
         console.log(chalk.yellow('No data found in the specified dataCollection.'));
         return;
@@ -85,12 +92,25 @@ export default {
 
       const foundData = dataCollection.filter((data: DataObject) => {
         for (const key in searchQuery) {
-          const queryValue = searchQuery[key].toString().toLowerCase();
-          const dataValue = data[key].toString().toLowerCase();
-          if (Number(dataValue) === searchQuery[key]) {
-            return true;
-          } else if (dataValue && dataValue.includes(queryValue)) {
-            return true;
+
+          const indexObject: IndexObject = dataObject[`${dataCollectionName}_${key}_index`];
+          const fieldValue = data[key];
+
+          if (indexObject && indexObject[key]) {
+            const indexedArray = indexObject[key]; //Array of indexes
+            const matchingIndexes = indexedArray.filter((index) => fieldValue === index);
+
+            if (matchingIndexes.length > 0) {
+              return true
+            }
+          } else {
+            const queryValue = searchQuery[key].toString().toLowerCase();
+            const dataValue = data[key].toString().toLowerCase();
+            if (Number(dataValue) === searchQuery[key]) {
+              return true;
+            } else if (dataValue && dataValue.includes(queryValue)) {
+              return true;
+            }
           }
         }
         return false;
@@ -123,7 +143,7 @@ export default {
     });
   },
 
-  edit(filePath: string, dataCollectionName: string, objectID: number, editObject: {[key: string]: string | number}): void {
+  edit(filePath: string, dataCollectionName: string, objectID: number, editObject: { [key: string]: string | number }): void {
     fs.readFile(filePath, 'utf8', (err, jsonData) => {
       if (err) {
         console.error(chalk.red('Error reading JSON file: ', err));
@@ -135,15 +155,49 @@ export default {
 
         const dataCollection = dataObject[dataCollectionName];
 
+        if (!dataCollection) {
+          console.log(chalk.red('no data found in the specified dataCollection.'));
+          return;
+        }
+
         const dataToEdit = dataCollection.find((data: DataObject) => data.id === objectID);
 
-        if (dataToEdit) {
-          for (const key in editObject) {
-            dataToEdit[key] = editObject[key];
-          }
-        } else {
-          console.log(chalk.red(`Dataset with ID ${objectID} not found.`));
+        if (!dataToEdit) {
+          console.log(chalk.red('No dataset found with the provided object ID.'));
           return;
+        }
+
+        let isIndexed = false;
+
+        for (const key in editObject) {
+          if (editObject.hasOwnProperty(key)) {
+            const fieldValue = editObject[key];
+
+            //Update the data field
+            dataToEdit[key] = fieldValue;
+
+            //Check if index object exists
+            const indexObject: IndexObject = dataObject[`${dataCollectionName}_${fieldValue}_index`];
+            
+            if (indexObject && indexObject[fieldValue]) {
+              const indexedArray = indexObject[fieldValue];
+              const matchingIndexes = indexedArray.filter((index) => index === fieldValue);
+
+              if (matchingIndexes.length > 0) {
+                indexedArray.push(objectID);
+              } 
+
+              isIndexed = true;
+
+            }
+          }
+        }
+
+        if (!isIndexed) {
+          //Fall back to regular search through the entire JSON file
+          for (const key in editObject) {
+            dataToEdit[key] = editObject[key]
+          }
         }
 
         const updatedJsonData = JSON.stringify(dataObject, null, 2);
@@ -154,7 +208,7 @@ export default {
             return;
           }
           console.log(chalk.green('JSON file has been updated successfully!'));
-        });
+        })
       } catch (err) {
         console.error(chalk.red('Error while finding data ' + err));
       }
@@ -204,6 +258,7 @@ export default {
       }
     });
   },
+
   del(filePath: string, dataCollectionName: string, objectID: number, deleteObject: object): void {
     fs.readFile(filePath, 'utf8', (err, jsonData) => {
       if (err) {
@@ -239,5 +294,56 @@ export default {
         console.error(chalk.red('Error while finding data ' + err));
       }
     });
+  },
+
+  //Indexing
+
+  createIndex(filePath: string, dataCollectionName: string, fieldName: string): void {
+    fs.readFile(filePath, 'utf8', (err, jsonData) => {
+      if (err) {
+        console.error(chalk.red('Error reading JSON data ' + err));
+        return;
+      }
+
+      try {
+        const dataObject: DataObject = JSON.parse(jsonData);
+        const dataCollection = dataObject[dataCollectionName];
+
+        if (!dataObject[`${dataCollectionName}_${fieldName}_index`]) {
+          dataObject[`${dataCollectionName}_${fieldName}_index`] = {};
+        }
+
+        const indexObject: IndexObject = dataObject[`${dataCollectionName}_${fieldName}_index`];
+
+        dataCollection.forEach((data: DataObject) => {
+          const fieldValue = data[fieldName];
+          if (fieldValue !== undefined) {
+            //Check if fieldValue is already a part of the indexed object.
+            if (!indexObject[fieldValue]) {
+              indexObject[fieldValue] = [];
+            }
+            if (indexObject[fieldValue].includes(data.id)) {
+              return;
+            }
+            indexObject[fieldValue].push(data.id);
+          }
+        });
+
+        const updatedJsonData = JSON.stringify(dataObject, null, 2);
+
+        fs.writeFile(filePath, updatedJsonData, (writeError) => {
+          if (writeError) {
+            console.error(chalk.red('Error while updating JSON file ' + err));
+            return;
+          }
+
+          console.log(chalk.green(`Successfully indexed "${fieldName}", under collection: ${dataCollectionName}`));
+        });
+
+      } catch (err) {
+        console.error(chalk.red('Error getting JSON data ' + err));
+      }
+    })
   }
 };
+
